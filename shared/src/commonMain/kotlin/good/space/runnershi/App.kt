25a198c471d6279e38b6b68b7e9b870f9d132312
Kim, Joon <kjoon418@naver.com>
2026-01-14 12:35:04 +0900

    chore(android): deprecated된 코드 개선

--- shared/src/commonMain/kotlin/good/space/runnershi/App.kt
+++ shared/src/commonMain/kotlin/good/space/runnershi/App.kt
@@ -12,89 +12,86 @@ import good.space.runnershi.ui.result.ResultRoute
 import good.space.runnershi.ui.running.RunningRoute
 import good.space.runnershi.ui.signup.SignUpRoute
 import good.space.runnershi.ui.theme.RunnersHiTheme
-import org.koin.compose.KoinContext
 
 @Composable
 fun App() {
-    KoinContext {
-        RunnersHiTheme {
-            val navController = rememberNavController()
+    RunnersHiTheme {
+        val navController = rememberNavController()
 
-            NavHost(
-                navController = navController,
-                startDestination = Screen.Login.name
-            ) {
-                // 로그인 화면
-                composable(route = Screen.Login.name) {
-                    LoginRoute(
-                        navigateToHome = {
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Login.name
+        ) {
+            // 로그인 화면
+            composable(route = Screen.Login.name) {
+                LoginRoute(
+                    navigateToHome = {
+                        navController.navigate(Screen.Home.name) {
+                            popUpTo(Screen.Login.name) { inclusive = true }
+                        }
+                    },
+                    navigateToSignUp = {
+                        navController.navigate(Screen.SignUp.name)
+                    }
+                )
+            }
+
+            // 회원가입 화면
+            composable(route = Screen.SignUp.name) {
+                SignUpRoute(
+                    navigateBack = {
+                        navController.popBackStack() // 뒤로 가기
+                    },
+                    navigateToHome = {
+                        navController.navigate(Screen.Home.name)
+                    }
+                )
+            }
+
+            // 홈 화면
+            composable(route = Screen.Home.name) {
+                HomeRoute(
+                    navigateToRunning = {
+                        navController.navigate(Screen.RUNNING.name)
+                    }
+                )
+            }
+
+            // 러닝 화면
+            composable(route = Screen.RUNNING.name) {
+                RunningRoute(
+                    navigateToResult = { userInfo, runResult ->
+                        // 싱글톤 홀더에 데이터 직접 주입
+                        ResultDataHolder.userInfo = userInfo
+                        ResultDataHolder.runResult = runResult
+
+                        // 결과 화면으로 이동
+                        navController.navigate(Screen.RESULT.name)
+                    }
+                )
+            }
+
+            // 결과 화면
+            composable(route = Screen.RESULT.name) {
+                ResultRoute(
+                    onCloseClick = {
+                        // 홈 화면까지 모든 화면을 제거하고 홈으로 이동
+                        if (!navController.popBackStack(Screen.Home.name, inclusive = false)) {
+                            // Home이 백스택에 없으면 navigate로 이동
                             navController.navigate(Screen.Home.name) {
-                                popUpTo(Screen.Login.name) { inclusive = true }
-                            }
-                        },
-                        navigateToSignUp = {
-                            navController.navigate(Screen.SignUp.name)
-                        }
-                    )
-                }
-
-                // 회원가입 화면
-                composable(route = Screen.SignUp.name) {
-                    SignUpRoute(
-                        navigateBack = {
-                          navController.popBackStack() // 뒤로 가기
-                        },
-                        navigateToHome = {
-                            navController.navigate(Screen.Home.name)
-                        }
-                    )
-                }
-
-                // 홈 화면
-                composable(route = Screen.Home.name) {
-                    HomeRoute(
-                        navigateToRunning = {
-                            navController.navigate(Screen.RUNNING.name)
-                        }
-                    )
-                }
-
-                // 러닝 화면
-                composable(route = Screen.RUNNING.name) {
-                    RunningRoute(
-                        navigateToResult = { userInfo, runResult ->
-                            // 싱글톤 홀더에 데이터 직접 주입
-                            ResultDataHolder.userInfo = userInfo
-                            ResultDataHolder.runResult = runResult
-                            
-                            // 결과 화면으로 이동
-                            navController.navigate(Screen.RESULT.name)
-                        }
-                    )
-                }
-
-                // 결과 화면
-                composable(route = Screen.RESULT.name) {
-                    ResultRoute(
-                        onCloseClick = {
-                            // 홈 화면까지 모든 화면을 제거하고 홈으로 이동
-                            if (!navController.popBackStack(Screen.Home.name, inclusive = false)) {
-                                // Home이 백스택에 없으면 navigate로 이동
-                                navController.navigate(Screen.Home.name) {
-                                    popUpTo(Screen.Home.name) { inclusive = false }
-                                }
-                            }
-                        },
-                        onDataMissing = {
-                            // 데이터가 없을 경우에 대한 안전장치
-                            if (!navController.popBackStack(Screen.Home.name, inclusive = false)) {
-                                navController.navigate(Screen.Home.name) {
-                                    popUpTo(Screen.Home.name) { inclusive = true }
-                                }
+                                popUpTo(Screen.Home.name) { inclusive = false }
                             }
                         }
-                    )
-                }
+                    },
+                    onDataMissing = {
+                        // 데이터가 없을 경우에 대한 안전장치
+                        if (!navController.popBackStack(Screen.Home.name, inclusive = false)) {
+                            navController.navigate(Screen.Home.name) {
+                                popUpTo(Screen.Home.name) { inclusive = true }
+                            }
+                        }
+                    }
+                )
             }
         }
     }
