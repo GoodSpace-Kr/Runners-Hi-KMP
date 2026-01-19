a9c490c679dcf65bd3fcdfbfc9287a61dd0e7838
Kim, Joon <kjoon418@naver.com>
2026-01-19 19:50:41 +0900

    feat(android): 앱 아이콘 변경

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "good.space.runnershi"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         manifestPlaceholders["MAPS_API_KEY"] = mapsApiKey
 
